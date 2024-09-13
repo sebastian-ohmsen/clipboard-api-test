ab197d7d2b859e93d274a26cd40cd9eae49f6093
Sebastian Ohmsen <hello@sebastianohmsen.com>
2024-09-13 13:09:36 +0200

    Adds intial sass styling.

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/** @type {import('vite').UserConfig} */
+export default {
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
+    }
+}
