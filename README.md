1b632189cce810bc52e3239c5a507be555a8fcd8
Sebastian Ohmsen <hello@sebastianohmsen.com>
2024-09-16 11:33:27 +0200

    Adds README to the project.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Clipboard Test 
+
+Small project to test the clipboard API.
+
+The project uses [vite](https://vitejs.dev/) to build and run the project. 
+
+Type the following command to start the development server:
+
+```bash
+npx vite
+```
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import './index.scss';
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    const clipboardContentElem = 
+        document.getElementById('clipboardContent');
+
+    document.getElementById('copyBtn').addEventListener('click', ()=> {
+        clipboardContentElem.value += `\ncopyBtn pressed`;
+    });
+
+    document.getElementById('pasteBtn').addEventListener('click', ()=> {
+        clipboardContentElem.value += `\npaseteBtn pressed`;
+    });
+});
