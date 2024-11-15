c7018fc11075ff710f57a0fec6ff39d7d8b9472b
Sebastian Ohmsen <hello@sebastianohmsen.com>
2024-11-15 15:02:33 +0100

    Removes wrong check in copy function that prevented execution if the start position of the selected text was zero. Also changed the position of the value to be pasted to the current cursor position.

--- clipboardHandler.js
+++ clipboardHandler.js
@@ -4,86 +4,123 @@ import Browser from './lib/browser';
 import StateIndicator from './lib/stateIndicators';
 
 /**
+ * Removes parts of a text.
+ *
+ * @param {string} text
+ *      The source text
+ * @param {number} start
+ *      Start position of the part that should be deleted
+ * @param {number} end
+ *      End position of the part that should be deleted
+ * @returns
+ *      The text without the deleted part or the original text if start or end
+ *      values are invalid.
+ */
+const deleteTextPart = (text, start, end) => {
+    if (
+        start === end ||
+        end < start ||
+        start < 0 ||
+        start >= text.length ||
+        end < 0 ||
+        end > text.length
+    )
+        return text;
+
+    return text.substring(0, start) + text.substring(end, text.length);
+};
+
+/**
  * Handler for actions to write to or read from the local clipboard.
- * 
- * @param {HTMLDivElement} copyIndicator 
+ *
+ * @param {HTMLDivElement} copyIndicator
  *      DIV element that indicates whether the copy function is available
  * @param {HTMLDivElement} pasteIndicator
  *      DIV element that indicates whether the paste function is available
  * @param {HTMLTextAreaElement} ioElement
  *      Element to display the clipboard content or select text that should be
- *      transferred to the clipboard 
+ *      transferred to the clipboard
  */
 function ClipboardHandler(copyIndicator, pasteIndicator, ioElement) {
-
     let browser = new Browser();
     let copyState = new StateIndicator(copyIndicator);
     let pasteState = new StateIndicator(pasteIndicator);
 
     /**
-     * Appends the given text value to the ioElement
-     * 
+     * Inserts the given text value into the value of the ioElement at
+     * the current cursor position.
+     *
      * @param {String} value
-     *      The text to append to the IOElement 
+     *      The text to append to the IOElement
      */
-    function appendToIo(value) {
+    function insertIntoIOElement(value) {
         if (ioElement) {
-            ioElement.value += `\n${value}`;
+            var oldValue = deleteTextPart(
+                ioElement.value,
+                ioElement.selectionStart,
+                ioElement.selectionEnd
+            );
+            var newText = oldValue.substring(0, ioElement.selectionStart);
+            newText += value;
+            newText += oldValue.substring(
+                ioElement.selectionStart,
+                oldValue.length
+            );
+            ioElement.value = newText;
+
+            ioElement.selectionStart = ioElement.selectionStart + value.length;
+            ioElement.selectionEnd = ioElement.selectionStart + value.length;
         }
-    };
+    }
 
     /* We only use the permission API if Blink engine is used */
     if (browser.isBlink) {
-
-        // @ts-ignore - non standard / experimental 
-        navigator.permissions.query({ name: 'clipboard-read' })
-            .then(result => {
+        // @ts-ignore - non standard / experimental
+        navigator.permissions
+            .query({ name: 'clipboard-read' })
+            .then((result) => {
                 if (result.state === 'granted') {
                     pasteState.setActive();
-                }
-                else if (result.state === 'prompt') {
-                    navigator.clipboard.readText()
+                } else if (result.state === 'prompt') {
+                    navigator.clipboard
+                        .readText()
                         .then(() => {
-                            pasteState.setActive();;
-
+                            pasteState.setActive();
                         })
-                        .catch(reason => {
+                        .catch((reason) => {
                             pasteState.setInactive();
                             console.error(reason);
                         });
-                }
-                else {
+                } else {
                     pasteState.setInactive();
                 }
-
             })
-            .catch(reason => {
+            .catch((reason) => {
                 console.error(reason);
                 pasteState.setInactive();
             });
 
         // @ts-ignore - non standard / experimental
-        navigator.permissions.query({ name: 'clipboard-write' })
-            .then(result => {
+        navigator.permissions
+            .query({ name: 'clipboard-write' })
+            .then((result) => {
                 if (result.state === 'granted') {
-                    copyState.setActive();    
-                }
-                else if (result.state === 'prompt') {
-                    navigator.clipboard.writeText('')
+                    copyState.setActive();
+                } else if (result.state === 'prompt') {
+                    navigator.clipboard
+                        .writeText('')
                         .then(() => {
                             copyState.setActive();
                         })
-                        .catch(reason => {
+                        .catch((reason) => {
                             console.error(reason);
                             copyState.setInactive();
                         });
-                }
-                else {
+                } else {
                     copyState.setInactive();
                 }
-
             })
-            .catch(reason => {
+            .catch((reason) => {
                 console.error(reason);
                 copyState.setInactive();
             });
@@ -96,21 +133,23 @@ function ClipboardHandler(copyIndicator, pasteIndicator, ioElement) {
     }
 
     let copySelectedText = () => {
-        if (ioElement && ioElement.selectionStart) {
+        if (ioElement) {
             let start = ioElement.selectionStart;
             let end = ioElement.selectionEnd;
             navigator.clipboard
-                .writeText(ioElement.value.substring(start, end));
+                .writeText(ioElement.value.substring(start, end))
+                .catch((err) => console.error(err));
         }
     };
 
     let pasteClipboardContent = () => {
         if (ioElement) {
-            navigator.clipboard.readText()
-                .then(result => {
-                    appendToIo(result);
+            navigator.clipboard
+                .readText()
+                .then((result) => {
+                    insertIntoIOElement(result);
                 })
-                .catch(reason => {
+                .catch((reason) => {
                     console.error(reason);
                 });
         }
@@ -118,8 +157,8 @@ function ClipboardHandler(copyIndicator, pasteIndicator, ioElement) {
 
     /**
      * Handler for key-down events. Reacts to shortcuts for copy and paste.
-     * 
-     * @param {KeyboardEvent} event 
+     *
+     * @param {KeyboardEvent} event
      *      Incoming keyboard event
      */
     this.keyDownHandler = (event) => {
